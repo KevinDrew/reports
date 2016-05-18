570d8e4e44d46dfbf1f8a352cd881ccb701e1071
Kevin Drew <drewk538@gmail.com>
2016-05-19 08:02:47 +1000

    standardised object classes and added getContacts method

--- common/m.crm-company.php
+++ common/m.crm-company.php
@@ -7,33 +7,21 @@ class CRMCompany extends VWObject {
 		parent::__construct($id);
 	}
 
-	function getContacts() {
-		$q =  'SELECT c.id_number,
- 				c.fname,
- 				c.sname,
- 				c.position,
- 				c.department,
- 				c.devision,
- 				c.email,
- 				c.phone,
- 				c.primaryBD,
- 				c.MYOB,
- 				c.edm,
- 				c.note,
- 				c.key,
- 				c.active,
- 				c.mobile,
- 				c.calendar
-			from crm_contactsLink l 
-			left join crm_contact c on (c.id_number=l.contact_id)
-			where company_id='. $this->id_number;
+	function getContacts($sort='con.sname,con.fname') {
+		$q = "SELECT con.id_number
+			FROM crm_contactslink l
+			left join crm_contact con on con.id_number=l.contact_ID
+			WHERE company_id=". $this->id_number ."
+			order by $sort
+		";
+
+		$rows = array();
 		$dbr = Factory::Get('dbread');
 		$res = $dbr->query($q);
-		$rows = array();
-		while ($row = $res->fetch(2)) {
-			$rows[] = $row;
+
+		while($row = $res->fetch()) {
+			$rows[] = new CRMContact($row['id_number']);
 		}
 		return $rows;
 	}
-
 }
